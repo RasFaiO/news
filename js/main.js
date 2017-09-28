8bc7d88a5d4a588d89cd45aed2c27fa424053e77
RasFaiO <fylancheros1@misena.edu.co>
2017-09-27 21:41:28 -0500

    Actualizacion_

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-$(".button-collapse").sideNav();
+//barra de navegación
+$(".button-collapse").sideNav()
+//cantidad de texto de las noticias
+$('.card-content>p').each(function(){
+    $(this).html($(this).text().substring(0,230))
+})
+//carrusel de imágenes
+$('.carousel.carousel-slider').carousel({fullWidth: true})
+$('.carousel').carousel('next',0);
