4fd5f3c83dad19bcff63ab66eeb1777865c71b35
RasFaiO <fylancheros1@misena.edu.co>
2017-10-13 18:01:43 -0500

    build

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
-var gulp = require('gulp');
-var server = require('gulp-server-livereload');
- 
+var gulp = require('gulp'),
+server = require('gulp-server-livereload'),
+useref = require('gulp-useref'),
+gulpif = require('gulp-if'),
+uglify = require('gulp-uglify'),
+minifyCss = require('gulp-clean-css');
+
 gulp.task('news', function() {
   gulp.src('./')
     .pipe(server({
@@ -10,3 +14,15 @@ gulp.task('news', function() {
       port: 8500
     }));
 })
+gulp.task('build-html', function () {
+  return gulp.src(['*.html','pa*/*.html'])
+      .pipe(useref())
+      .pipe(gulpif('j*/*.js', uglify()))
+      .pipe(gulpif('style*/**', minifyCss()))
+      .pipe(gulp.dest('build'));
+})
+gulp.task('build-assets',function (){
+  return gulp.src('asset*/im*/**').
+  pipe(gulp.dest('build'))
+})
+gulp.task('build',['build-html','build-assets'])
